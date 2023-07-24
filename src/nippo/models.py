5f6c873db9c86253b04d4758495ebb68d093c83c
Mizuto <mizutokame07@gmail.com>
2023-07-24 23:58:34 +0900

    DBを設定した

--- src/nippo/models.py
+++ src/nippo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class NippoModel(models.Model):
+    title = models.CharField(max_length=100) # charfieldはテキスト
+    content = models.CharField(max_length=1000) # max_lengthは書き込める最大文字数 255推奨
+    timestamp = models.DateTimeField(auto_now_add=True) # 日付と時間を書きこむ
+    # auto_now_addをtrueで自動で今の時刻を書き込む
