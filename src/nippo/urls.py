ecd495af05573f138827b60a2f0cbef863fda3b8
Mizuto <mizutokame07@gmail.com>
2023-07-25 23:28:06 +0900

    templatesを追加しhtmlファイルを追加した

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import nippoListView # .は同じディレクトリ内を示す
  
 urlpatterns = [
-  
+  path("", nippoListView), # views.pyでつくった関数名
 ]
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def nippoListView(request): # requestは必ず引数で指定
+	return render(request, "nippo/nippo-list.html") # 第一引数にrequest 第二引数に表示したいhtml
